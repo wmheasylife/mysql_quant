df3964ef040686042f26e5466161b8c4757cc375
wmheasylife <wmheasylife@163.com>
2023-01-18 20:26:54 +0800

    支持多线程更新股票数据

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # mysql_quant
+
+## 1，更新股票日线数据到本地数据库
+### 文件
+```shell
+{dir}/download_data_stock.py
+```
+### 功能说明
+运行此脚本，每天从baostock 下载文件到本地mysql 数据库。
+支持断点下载。
+### 运行方式
+```python
+python3 download_data_stock.py
+```
+
+### 依赖文件
+```
+baostock_date_data.csv
+```
+### 依赖环境
+需要在本地构建mysql数据库，并且为数据库添加默认root 用户，root用户密码为 Meta#2023
+
+
+# 文件说明
+* .gitignore git工程通用文件，忽略被写入.gitignore文件的git工程目录下的文件或者文件夹
+* baostock_date_data.csv   baostock 平台给出日线数据的字段信息
+* minute_data.csv  baostock 平台给出分钟级别数据的字段信息
+* week_month_data.csv  baostock 平台给出周线、月线数据的字段信息
+* download_data_stock.py 更新股票日线数据到本地数据库脚本。可以在需要的时候更新本地数据库
+
+
+# 待更新
+
--- download_data_stock.py
+++ download_data_stock.py
@@ -4,14 +4,21 @@
 """
 import baostock as bs
 import datetime
-import sys
 import numpy as np
 import pandas as pd
 import multiprocessing
 import pymysql
 import logging
-import sqlalchemy
+import random
+import time
 import math
+
+
+
+def init_logging():
+    LOG_FORMAT = '%(asctime)s %(levelname)s %(process)d %(processName)s [%(filename)s:%(lineno)s] %(message)s'
+    # logging.basicConfig(filename='update_stock.log', level=logging.DEBUG, format=LOG_FORMAT)
+    logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
 
 
 def create_mysql_engine():
@@ -23,7 +30,7 @@ def create_mysql_engine():
     # 引擎参数信息
     host = 'localhost'
     user = 'root'
-    passwd = 'Web#rndcl75'
+    passwd = 'Meta#2023'
     port = 3306
     db = 'db_quant'
 
@@ -46,143 +53,51 @@ g_baostock_date_data_fields = 'date,open,high,low,close,preclose,volume,amount,a
 
 
 class UpdateStockDailyData:
-    def __init__(self) -> None:
-        self.stock_codes = list()
-        self.stock_newest_data_date = dict()
-        self.default_oldest_data_date =  '2009-02-28' # 查询Date数据时，不包含 from_date, 最早数据为2009-03-01
+    def __init__(self, stock_codes, column_data_type, adjustflag='2'):
+        # 异步进程处理，这里需要重新初始化一下 logging
+        # 
+        self.stock_codes = stock_codes
+        self.column_data_type = column_data_type
+        self.adjustflag = adjustflag
         self.mysql_quant_engine = create_mysql_engine()
-        self.column_data_type = dict()
 
-    def get_stock_codes(self, date=None):
-        """
-        获取指定日期的A股代码列表
-        若参数date为空，则返回最近1个交易日的A股代码列表
-        若参数date不为空，且为交易日，则返回date当日的A股代码列表
-        若参数date不为空，但不为交易日，则打印提示非交易日信息，程序退出
-        :param date: 日期
-        :return: A股代码的列表
-        """
+    def do_update(self):
+        return self.create_data(self.stock_codes)
 
-        # 登录baostock
-        bs.login()
-
-        # 从BaoStock查询股票数据
-        stock_df = bs.query_all_stock(date).get_data()
-
-        # 如果获取数据长度为0，表示日期date非交易日
-        if 0 == len(stock_df):
-
-            # 如果设置了参数date，则打印信息提示date为非交易日
-            if date is not None:
-                logging.info('当前选择日期为非交易日或尚无交易数据，请设置date为历史某交易日日期')
-                return False
-
-            # 未设置参数date，则向历史查找最近的交易日，当获取股票数据长度非0时，即找到最近交易日
-            delta = 1
-            today = datetime.date.today()
-            while 0 == len(stock_df):
-                stock_df = bs.query_all_stock(today - datetime.timedelta(days=delta)).get_data()
-                delta += 1
-
-        # 注销登录
-        bs.logout()
-
-        # 筛选股票数据，上证和深证股票代码在sh.600000与sz.39900之间
-        stock_df = stock_df[(stock_df['code'] >= 'sh.600000') & (stock_df['code'] < 'sz.399000')]
-
-        # 返回股票列表
-        self.stock_codes = stock_df['code'].tolist()
-        # self.stock_codes = ['sh.600110']
-
-        logging.info('下载最近的股票列表成功! 股票数量为 %s' %(len(self.stock_codes)))
-        return True
-
-    def get_stock_newest_date(self, stock_codes=list()):
-        """
-        从数据库中获取最新股票的日线数据日期。如果对应的股票不存在最新的日线数据，
-        那么返回(字符串类型的)2009-03-01
-        """
-        cursor = self.mysql_quant_engine.cursor()
-        ret = cursor.execute('select table_name from information_schema.tables where table_schema="db_quant"')
-        table_name_result = cursor.fetchall()
-        table_list = list()
-        for table_name in table_name_result:
-            table_list.append(table_name['TABLE_NAME'])
-        logging.debug('table_list is :' + str(table_list))
-        logging.basicConfig()
-        temp_df = pd.read_csv('baostock_date_data.csv')
-        for index,column_info in temp_df.iterrows():
-            # create_table_sql += column_info['参数名称'] + ' ' + column_info['data_type'] + ','
-            self.column_data_type[column_info['参数名称']] = column_info['data_type']
-
-        for stock_code in stock_codes:
-            table_name = '{}_{}'.format(stock_code[3:], stock_code[:2])
-            if table_name in table_list:
-                get_stock_date_sql = 'select date from ' + table_name + ' order by date desc limit 1'
-                logging.debug(get_stock_date_sql)
-                cursor.execute(get_stock_date_sql)
-                ret = cursor.fetchall()
-                if len(ret) == 0:
-                    self.stock_newest_data_date[stock_code] = self.default_oldest_data_date
-                else:
-                    # 找到最新的数据
-                    for item in ret:
-                       self.stock_newest_data_date[stock_code] = (item['date'] + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
-                continue
-            else:
-                self.stock_newest_data_date[stock_code] = self.default_oldest_data_date
-                create_table_sql= 'create table %s (' % (table_name)
-                temp_df = pd.read_csv('baostock_date_data.csv')
-                for col_name,data_type in self.column_data_type.items():
-                    create_table_sql += col_name + ' ' + data_type + ','
-
-                create_table_sql += 'index(date)'
-                create_table_sql += ')'
-                logging.info('will execute create table sql with sql [' + create_table_sql + ']')
-                cursor.execute(create_table_sql)
-        logging.debug('self.stock_newest_data_date is : ' + str(self.stock_newest_data_date))
-        cursor.close()
-        self.mysql_quant_engine.commit()
-
-
-    def create_data(self, stock_codes, adjustflag='2'):
+    def create_data(self, stock_codes):
         """
         下载指定日期内，指定股票的日线数据
         :param stock_codes: 待下载数据的股票代码
-        :param adjustflag: 复权选项 1：后复权  2：前复权  3：不复权  默认为前复权
         :return: None
         """
 
+        all_stock_num = len(stock_codes)
         logging.info('调用 create_data 函数，传入了 ' + str(len(stock_codes)) + ' 个股票!')
+        if all_stock_num == 0:
+            return True
 
-
-        # 创建数据库引擎对象
+        # 登录 baostock
         bs.login()
-
 
         # 下载股票循环
         cursor = self.mysql_quant_engine.cursor()
         to_date = datetime.datetime.today().strftime('%Y-%m-%d')
         # to_date = '2022-10-30'
-        all_stock_num = len(stock_codes)
+
         for i in range(all_stock_num):
-            stock_code = stock_codes[i]
-            from_date = self.stock_newest_data_date[stock_code]
+            stock_code = stock_codes[i][0] # stock_code
+            from_date = stock_codes[i][1] # stock_code new date
             today_str = datetime.datetime.today().strftime('%Y-%m-%d')
             if from_date > today_str:
                 logging.info('[{}/{}] {} 的日线数据最新日期为{}，无需更新!'.format(i + 1, all_stock_num, stock_code, from_date))
                 continue
-            logging.info('[{}/{}] 正在更新{}日线数据 . from {} ~ {}'.format(i + 1, all_stock_num, stock_code, from_date, to_date))
+            logging.info('[{}/{}] 正在更新 {} 日线数据 from {} ~ {}'.format(i + 1, all_stock_num, stock_code, from_date, to_date))
 
-            # 登录BaoStock
-            # # 能否放在外部??
-            # bs.login()
 
             # 下载日线数据
             out_df = bs.query_history_k_data_plus(stock_code, g_baostock_date_data_fields,
                                                   start_date=from_date, end_date=to_date,
-                                                  frequency='d', adjustflag=adjustflag).get_data()
-
+                                                  frequency='d', adjustflag=self.adjustflag).get_data()
 
             code_column_list = [stock_code for i in range(len(out_df.index))]
             out_df['code'] = code_column_list
@@ -239,10 +154,115 @@ class UpdateStockDailyData:
             self.mysql_quant_engine.commit()
         cursor.close()
         bs.logout()
-        # self.mysql_quant_engine.commit()
+        return True
+
+class GenerateStockCodeGroup:
+
+    def __init__(self) -> None:
+        self.stock_codes = list()
+        self.stock_newest_data_date = dict()
+        self.default_oldest_data_date =  '2009-02-28' # 查询Date数据时，不包含 from_date, 最早数据为2009-03-01
+        self.mysql_quant_engine = create_mysql_engine()
+        self.column_data_type = dict()
+
+    def get_stock_codes(self, date=None):
+        """
+        获取指定日期的A股代码列表
+        若参数date为空，则返回最近1个交易日的A股代码列表
+        若参数date不为空，且为交易日，则返回date当日的A股代码列表
+        若参数date不为空，但不为交易日，则打印提示非交易日信息，程序退出
+        :param date: 日期
+        :return: A股代码的列表
+        """
+
+        # 登录baostock
+        bs.login()
+
+        # 从BaoStock查询股票数据
+        stock_df = bs.query_all_stock(date).get_data()
+
+        # 如果获取数据长度为0，表示日期date非交易日
+        if 0 == len(stock_df):
+
+            # 如果设置了参数date，则打印信息提示date为非交易日
+            if date is not None:
+                logging.info('当前选择日期为非交易日或尚无交易数据，请设置date为历史某交易日日期')
+                return False
+
+            # 未设置参数date，则向历史查找最近的交易日，当获取股票数据长度非0时，即找到最近交易日
+            delta = 1
+            today = datetime.date.today()
+            while 0 == len(stock_df):
+                stock_df = bs.query_all_stock(today - datetime.timedelta(days=delta)).get_data()
+                delta += 1
+
+        # 注销登录
+        bs.logout()
+
+        # 筛选股票数据，上证和深证股票代码在sh.600000与sz.39900之间
+        stock_df = stock_df[(stock_df['code'] >= 'sh.600000') & (stock_df['code'] < 'sz.399000')]
+
+        # 返回股票列表
+        self.stock_codes = stock_df['code'].tolist()
+        # self.stock_codes = ['sh.600110']
+
+        logging.info('下载最近的股票列表成功! 股票数量为 %s' %(len(self.stock_codes)))
+        return True
+
+    def get_stock_newest_date(self):
+        """
+        从数据库中获取最新股票的日线数据日期。如果对应的股票不存在最新的日线数据，
+        那么返回(字符串类型的)2009-03-01
+        """
+        cursor = self.mysql_quant_engine.cursor()
+        ret = cursor.execute('select table_name from information_schema.tables where table_schema="db_quant"')
+        table_name_result = cursor.fetchall()
+        table_list = list()
+        for table_name in table_name_result:
+            table_list.append(table_name['TABLE_NAME'])
+        logging.debug('table_list is :' + str(table_list))
+        logging.basicConfig()
+        temp_df = pd.read_csv('baostock_date_data.csv')
+        for index,column_info in temp_df.iterrows():
+            # create_table_sql += column_info['参数名称'] + ' ' + column_info['data_type'] + ','
+            self.column_data_type[column_info['参数名称']] = column_info['data_type']
+
+        for stock_code in self.stock_codes:
+            table_name = '{}_{}'.format(stock_code[3:], stock_code[:2])
+            if table_name in table_list:
+                get_stock_date_sql = 'select date from ' + table_name + ' order by date desc limit 1'
+                logging.debug(get_stock_date_sql)
+                cursor.execute(get_stock_date_sql)
+                ret = cursor.fetchall()
+                if len(ret) == 0:
+                    self.stock_newest_data_date[stock_code] = self.default_oldest_data_date
+                else:
+                    # 找到最新的数据
+                    today_str = datetime.datetime.today().strftime('%Y-%m-%d')
+                    for item in ret:
+                        code_next_date_str = (item['date'] + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+                        if code_next_date_str > today_str:
+                            logging.info(stock_code + ' has newest trate data, with next_date_str:' + code_next_date_str)
+                            continue
+                        self.stock_newest_data_date[stock_code] = (item['date'] + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+                continue
+            else:
+                self.stock_newest_data_date[stock_code] = self.default_oldest_data_date
+                create_table_sql= 'create table %s (' % (table_name)
+                temp_df = pd.read_csv('baostock_date_data.csv')
+                for col_name,data_type in self.column_data_type.items():
+                    create_table_sql += col_name + ' ' + data_type + ','
+
+                create_table_sql += 'index(date)'
+                create_table_sql += ')'
+                logging.info('will execute create table sql with sql [' + create_table_sql + ']')
+                cursor.execute(create_table_sql)
+        logging.debug('self.stock_newest_data_date is : ' + str(self.stock_newest_data_date))
+        cursor.close()
+        self.mysql_quant_engine.commit()
 
 
-    def get_code_group(self, process_num, stock_codes):
+    def get_code_group(self, process_num):
         """
         获取代码分组，用于多进程计算，每个进程处理一组股票
 
@@ -252,71 +272,73 @@ class UpdateStockDailyData:
         """
 
         # 创建空的分组
-        code_group = [[] for i in range(process_num)]
+        all_stock_number = len(list(self.stock_newest_data_date.keys()))
+        code_group = [[] for i in range(all_stock_number)]
 
         # 按余数为每个分组分配股票
-        for index, code in enumerate(stock_codes):
-            code_group[index % process_num].append(code)
-
+        index = 0
+        for stock_code,stock_new_date in self.stock_newest_data_date.items():
+            code_group[index % process_num].append((stock_code, stock_new_date))
+            index += 1
 
         logging.debug('code group number is : ' + str(len(code_group)))
         return code_group
 
+    def get_columns_data_type(self):
+        return self.column_data_type
 
-    def multiprocessing_func(self, func, args):
-        """
-        多进程调用函数
+def do_one_group_update_stock(i, stock_codes, columns_data_type, adjustflag='2'):
+    """
+    独立函数，异步更新 stock 日线数据的入口
 
-        :param func: 函数名
-        :param args: func的参数，类型为元组，第0个元素为进程数，第1个元素为股票代码列表
-        :return: 包含各子进程返回对象的列表
-        """
+    :param stock_codes 这个进程需要处理股票的列表，以及股票日线数据的起始日期
+    :param clumns_data_type 这个时对应股票列表的数据类型
+    :param adjustflag  复权选项 1：后复权  2：前复权  3：不复权  默认为前复权
+    """
+    init_logging()
+    logging.info('will run do_one_group_update_stock')
+    # time.sleep(random.randint(1 * 10))
+    try:
+        update_lock_stock = UpdateStockDailyData(stock_codes, columns_data_type, adjustflag)
+        succ = update_lock_stock.do_update()
+    except Exception as e:
+        logging.exception(e) # 这里，异步进程异常退出时， stdout 看不到任何东西，这里使用logging.exception(e) 输出为log
+        succ = False
 
-        logging.info('call multiprocessing_func')
-        print(func)
+    return (i, succ)
 
-        results = list()
-        # 创建进程池
-        with multiprocessing.Pool(processes=args[0]) as pool:
-            # 多进程拉取股票时间
-            for codes in self.get_code_group(args[0], args[1]):
-                results.append(pool.apply_async(func, args=(codes, *args[2:],)))
+def callback_update_stock(result):
+    logging.info('update stock result is :' + str(result))
 
-            # 阻止后续任务提交到进程池
-            pool.close()
+def create_data_mp(process_num=5, adjustflag='2'):
+    """
+    多进程调用函数
 
-            # 等待所有进程结束
-            pool.join()
+    :param funcprocess_num: 期望进程数量，最好和CPU核心数相同
+    :param adjustflag: 复权选项 1：后复权  2：前复权  3：不复权  默认为前复权
+    """
 
+    logging.info('call multiprocessing_func')
 
+    results = list()
+    # 创建进程池
+    pool =  multiprocessing.Pool(processes=process_num)
+    # 多进程拉取股票时间
+    temp_generater = GenerateStockCodeGroup()
+    temp_generater.get_stock_codes()
+    temp_generater.get_stock_newest_date()
+    columns_data_type = temp_generater.get_columns_data_type()
+    index = 0
+    for codes in temp_generater.get_code_group(process_num):
+        logging.info('will apply async process. do_one_group_update_stock' + str(codes[:10]) + ' with index:' + str(index))
+        pool.apply_async(do_one_group_update_stock, args=(index, codes, columns_data_type, adjustflag), callback=callback_update_stock) 
+    pool.close()
+    pool.join()   #调用join之前，先调用close函数，否则会出错。执行完close后不会有新的进程加入到pool,join函数等待所有子进程结束
+    print("Sub-process(es) done.")
 
-    def create_data_mp(self, stock_codes, process_num=5, adjustflag='2'):
-        """
-        使用多进程创建指定日期内，指定股票的日线数据，计算扩展因子
-
-        :param stock_codes: 待创建数据的股票代码
-        :param process_num: 进程数
-        :param from_date: 日线开始日期
-        :param to_date: 日线结束日期
-        :param adjustflag: 复权选项 1：后复权  2：前复权  3：不复权  默认为前复权
-        :return: None
-        """
-
-        self.multiprocessing_func(UpdateStockDailyData.create_data, (process_num, stock_codes, adjustflag,))
-
-    def do_update(self):
-        self.get_stock_codes()
-        self.get_stock_newest_date(self.stock_codes)
-        self.create_data(self.stock_codes)
-        # self.create_data_mp(self.stock_codes)
 
 
 if __name__ == '__main__':
-    LOG_FORMAT = '%(asctime)s %(levelname)s [%(filename)s:%(lineno)s] %(message)s'
-    logging.basicConfig(filename='update_stock.log', level=logging.DEBUG, format=LOG_FORMAT)
-	# logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
+    init_logging()
     logging.info('Finish init logging!')
-    update_lock_stock = UpdateStockDailyData()
-    update_lock_stock.do_update()
-    # stock_codes = get_stock_codes()
-    # create_data_mp(stock_codes[:5])
+    create_data_mp(5, '2')
